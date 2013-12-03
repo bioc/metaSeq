d3f37dc28ad6b396b08dbb376d993fa614a7b6eb
Koki Tsuyuzaki <k.t.the-answer@hotmail.co.jp>
2013-12-03 03:08:34 +0000

    Version 1.3.2
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/metaSeq@83837 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/Accelerate.NOISeq.R
+++ R/Accelerate.NOISeq.R
@@ -1,11 +1,25 @@
 Accelerate.NOISeq <-
-function () 
+function (OS = NULL) 
 {
-    data(text.n.menor)
-    data(text.busca)
-    env <- getNamespace("NOISeq")
-    sourceCpp(code = text.n.menor)
-    sourceCpp(code = text.busca)
-    assignInNamespace("busca", busca, ns = "NOISeq", envir = env)
-    assignInNamespace("n.menor", nmenor, ns = "NOISeq", envir = env)
+    if (OS == "Unix") {
+        data(text.n.menor_unix)
+        data(text.busca_unix)
+        env <- getNamespace("NOISeq")
+        sourceCpp(code = text.n.menor_unix)
+        sourceCpp(code = text.busca_unix)
+        assignInNamespace("busca", busca, ns = "NOISeq", envir = env)
+        assignInNamespace("n.menor", nmenor, ns = "NOISeq", envir = env)
+    }
+    if (OS == "Windows") {
+        data(text.n.menor_win)
+        data(text.busca_win)
+        env <- getNamespace("NOISeq")
+        sourceCpp(code = text.n.menor_win)
+        sourceCpp(code = text.busca_win)
+        assignInNamespace("busca", busca, ns = "NOISeq", envir = env)
+        assignInNamespace("n.menor", nmenor, ns = "NOISeq", envir = env)
+    }
+    else {
+        warning("Please specify OS as Unix or Windows!")
+    }
 }
